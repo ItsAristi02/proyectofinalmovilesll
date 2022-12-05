eff4745392bb69319663ea3b8a46bf9ca6b30074
206 <206@academicos.cesde.edu.co>
2022-12-05 11:02:35 -0500

    Se crean interfaces, metodos de servicios y listas de almacenamientó

--- src/main/java/com/example/viaticosonline/repositorios/RepositorioViaje.java
+++ src/main/java/com/example/viaticosonline/repositorios/RepositorioViaje.java
@@ -4,4 +4,5 @@ import com.example.viaticosonline.entidades.Viaje;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RepositorioViaje extends JpaRepository<Viaje, Integer> {
+
 }
